0c7f5ea633fd7b2951eed75a54967eba7b1b0106
Maureen Ougo <maureen.ougo@student.moringaschool.com>
2021-10-30 17:00:52 +0300

    adding email.py

--- neighborhood/settings.py
+++ neighborhood/settings.py
@@ -85,10 +85,13 @@ WSGI_APPLICATION = 'neighborhood.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#......
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neighborhood',
+        'USER': 'moringa',
+    'PASSWORD':'mymaureen',
     }
 }
 
