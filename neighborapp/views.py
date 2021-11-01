81e3c457622d4d4ae0c555e70aa2c2fad3762469
Maureen Ougo <maureen.ougo@student.moringaschool.com>
2021-11-01 18:20:12 +0300

    modifying templates

--- neighborapp/views.py
+++ neighborapp/views.py
@@ -1,79 +1,83 @@
-from django.shortcuts import render,redirect
-from django.http import HttpResponse,Http404,HttpResponseRedirect
-from .models import UserProfile,Post,Neighborhood,Business,Comment
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from .models import UserProfile, Post, Neighborhood, Business, Comment
 from django.contrib.auth.decorators import login_required
 import datetime as dt
-from .forms import UserProfileForm,BusinessForm,PostForm,CommentForm
+from .forms import UserProfileForm, BusinessForm, PostForm, CommentForm
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializer import BusinessSerializer
 from .email import send_amber_email
 
 # Create your views here.
-@login_required
+
+
+@login_required(login_url='/accounts/login/')
 def index(request):
     current_user = request.user
-    # try:
-    #     
-    # except:
-    #     return redirect("index")
-
-    # try:
-    profile = UserProfile.objects.get(user = current_user)
-    posts = Post.objects.filter(neighborhood = profile.neighborhood)
-    # except:
-    #     posts = None
-    
+    form = PostForm()
     if request.method == 'POST':
-        form = PostForm(request.POST)
+        form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             post = form.save(commit=False)
             post.user = current_user
-            post.neighborhood = profile.neighborhood
+            # check if user exists and if not set neighborhood to None
+            if UserProfile.objects.filter(user_id=current_user.id).exists():
+                profile = UserProfile.objects.get(user_id=current_user.id)
+                post.neighborhood = profile.neighborhood
+            else:
+                post.neighborhood = 1
             post.type = request.POST['type']
             post.save()
-
-            if post.type == '1':
-                recipients = UserProfile.objects.filter(neighborhood=post.neighborhood)
-                for recipient in recipients:
-                    send_amber_email(post.title,post.content,recipient.email)
-
         return redirect('index')
     else:
         form = PostForm()
-    return render(request,'index.html',{"posts":posts,"profile":profile,"form":form})
+
+    if UserProfile.objects.filter(user_id=current_user.id).exists():
+        profile = UserProfile.objects.get(user_id=current_user.id)
+        if profile.neighborhood:
+            neighborhood = Neighborhood.objects.get(id=profile.neighborhood.id)
+            posts = Post.objects.filter(neighborhood=neighborhood)
+            businesses = Business.objects.filter(neighborhood=neighborhood)
+            return render(request, 'index.html', {"posts": posts, "businesses": businesses, "profile": profile, "form": form})
+        else:
+            
+            return render(request, 'index.html', {"profile": profile, "form": form})
+    else:
+        return render(request, 'index.html', {"form": form})
 
 
 @login_required(login_url='/accounts/login/')
 def edit_profile(request):
     date = dt.date.today()
     current_user = request.user
-    profile = UserProfile.objects.objects.filter(user_id=current_user.id).first()
+    profile = UserProfile.objects.objects.filter(
+        user_id=current_user.id).first()
     posts = Post.objects.filter(user_id=current_user.id)
     if request.method == 'POST':
-        signup_form = UserProfileForm(request.POST, request.FILES,instance=request.user.profile) 
+        signup_form = UserProfileForm(
+            request.POST, request.FILES, instance=request.user.profile)
         if signup_form.is_valid():
             signup_form.save()
             return redirect('profile')
     else:
-        signup_form =UserProfileForm() 
-        
+        signup_form = UserProfileForm()
 
-    
-    return render(request, 'profile/edit_profile.html', {"date": date, "form":signup_form,"profile":profile, "posts":posts})
+    return render(request, 'profile/edit_profile.html', {"date": date, "form": signup_form, "profile": profile, "posts": posts})
+
 
 def profile(request):
     date = dt.date.today()
     current_user = request.user
     profile = UserProfile.objects.get(user=current_user.id)
     posts = Post.objects.filter(user=current_user)
-    return render(request, 'profile/profile.html', {"date": date, "profile":profile, "posts":posts})
+    return render(request, 'profile/profile.html', {"date": date, "profile": profile, "posts": posts})
 
 
 @login_required(login_url='/accounts/login/')
 def businesses(request):
     current_user = request.user
-    neighborhood = UserProfile.objects.get(user = current_user).neighborhood
+    neighborhood = UserProfile.objects.get(user=current_user).neighborhood
     if request.method == 'POST':
         form = BusinessForm(request.POST)
         if form.is_valid():
@@ -86,13 +90,14 @@ def businesses(request):
         form = BusinessForm()
 
     try:
-        businesses = Business.objects.filter(neighborhood = neighborhood)
+        businesses = Business.objects.filter(neighborhood=neighborhood)
     except:
         businesses = None
 
-    return render(request,'business.html',{"businesses":businesses,"form":form})
+    return render(request, 'business.html', {"businesses": businesses, "form": form})
 
-def post(request,id):
+
+def post(request, id):
     post = Post.objects.get(id=id)
     comments = Comment.objects.filter(post=post)
     if request.method == 'POST':
@@ -102,10 +107,11 @@ def post(request,id):
             comment.user = request.user
             comment.post = post
             comment.save()
-        return redirect('post',id = post.id)
+        return redirect('post', id=post.id)
     else:
         form = CommentForm()
-    return render(request,'post.html',{"post":post,"comments":comments,"form":form})
+    return render(request, 'post.html', {"post": post, "comments": comments, "form": form})
+
 
 class BusinessList(APIView):
     def get(self, request, format=None):
@@ -119,7 +125,7 @@ def search(request):
     if 'search' in request.GET and request.GET["search"]:
         search_term = request.GET.get("search")
         businesses = Business.objects.filter(name__icontains=search_term)
-        return render(request,'search.html',{'businesses':businesses})
+        return render(request, 'search.html', {'businesses': businesses})
     else:
         message = "You haven't searched for any term"
-        return render(request, 'search.html',{"message":message})
+        return render(request, 'search.html', {"message": message})
